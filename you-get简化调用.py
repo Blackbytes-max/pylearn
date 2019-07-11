f25a93071be9c7b4836368e89aa041a1db78d2a8
Blackbytes-max <52771689+Blackbytes-max@users.noreply.github.com>
2019-07-11 17:29:03 +0800

    Update you-get简化调用.py

--- you-get简化调用.py
+++ you-get简化调用.py
@@ -1,6 +1,9 @@
+# !/usr/bin/python3
 #you-get快速调用脚本
 import os
 links=input('请输入网页链接： ')
-cmdstr="you-get "+links
+os.chdir(os.environ["HOMEPATH"]+"\\Downloads")
+cmdstr="you-get  "+links
 os.system(cmdstr)
+input('按下ENTER关闭本窗口')
 exit()
